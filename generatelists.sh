342cb0a59cab402432ab65d44e0eb728927f39bf
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-11-01 16:14:22 -0500

    errant a

--- generatelists.sh
+++ generatelists.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -eux
 cut -f2 -d, data/nounlist.csv > alternative-nouns/long.list
-cut -f2 -d, data/Adjectives.csv |tr -d '\r'|sed 's/^\s*\(\S*\)/\1/' > alternative-adjectives/long.list
+cut -f2 -d, data/Adjectives.csv \
+  |tr -d '\r' \
+  |sed 's/^\s*\(\S*\)/\1/' \
+  |grep -v '^Word$' \
+  |grep -v '^a$' \
+  > alternative-adjectives/long.list
 cut -f1 -d, data/greek_gods.csv > alternative-nouns/greek_gods.list
 cat alternative-nouns/*.list > nouns/full.list
 cat alternative-adjectives/*.list > adjectives/full.list
