1afa64cacde0be6eeb0246644d64388658725e41
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-11-01 16:07:11 -0500

    generate2

--- generatelists.sh
+++ generatelists.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+set -eux
 cut -f2 -d, data/nounlist.csv > ../alternative-nouns/long.list
-cut -f2 -d, data/Adjectives.csv > alternatives/long.list
-cut -f1 -d, data/greek_gods.csv > greek_gods
+cut -f2 -d, data/Adjectives.csv > alternative-adjectives/long.list
+cut -f1 -d, data/greek_gods.csv > alternative-nouns/greek_gods.list
 cat alternative-nouns/*.list > nouns/full.list
 cat alternative-adjectives/*.list > adjectives/full.list
