7235eadddea449911fc6d06a5dcc2c327f492160
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-11-17 13:03:10 -0600

    test.sh

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+: "${counto:=24}"
+: ${TEST:="make test"}
+: ${MESSAGE:="/test ${TEST}"}
+#: "${MODEL:=ollama_chat/qwen2.5-coder:32b-base-fp16}"
+#: "${MODEL:=ollama_chat/qwen3:32b-fp16}"
+#: "${MODEL:=ollama_chat/llama4:scout}"
+#: ${MODEL:=ollama_chat/gemma3:27b-it-fp16}
+: ${MODEL:=ollama_chat/gpt-oss:120b}
+#: ${MODEL:=ollama_chat/deepseek-r1:70b-llama-distill-q8_0}
+#: ${MODEL:=ollama_chat/granite4:32b-a9b-h}
+$TEST
+if [[ ! $? -eq 0 ]]; then
+  echo "$i  <=======================" 
+  aider \
+    --config config/shootout.conf.yml \
+    --model "ollama_chat/${MODEL}" \
+    --test-cmd ${TEST} \
+    -m "${MESSAGE}"
+  ((countzero++))
+  $TEST
+  if [[ $? -eq 0 ]]; then
+    break
+  fi
+fi
